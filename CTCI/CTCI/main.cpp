371965399fbc4f5a1dd6513831ffa284ee184c13
Jessica Joseph <Jessicajoseph@nyu.edu>
2016-04-07 13:57:27 -0400

    NOTES and Solutions

---
+++ CTCI/CTCI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CTCI
+//
+//  Created by Jessica Joseph on 4/7/16.
+//  Copyright © 2016 Jessica Joseph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
